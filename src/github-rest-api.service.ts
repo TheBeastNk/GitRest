b87a81c9b8ba3a2f9f43ee21583a4fa11c60cca7
TheBeastNk <niranjans97@gmail.com>
2018-11-06 08:14:32 +0530

    First Commit

---
+++ src/github-rest-api.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import 'rxjs/add/operator/map';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GithubRestApiService {
+  public gitUsersData: any;
+  public gitUserDetails: any;
+  public getGithubUsersByName;
+
+  constructor(private httpServ: Http) { }
+}
+
+  //method to fetch Github users data from REST Api by making http request
+  getGithubUsersByName(searchName) {
+    return new Promise(resolve => {
+      this.httpServ.get("https://api.github.com/search/users?q=" + searchName)
+        .map(resp => resp.json())
+        .subscribe(tempdata => {
+          this.gitUsersData = tempdata;
+          resolve(this.gitUsersData);
+        });
+    });
+  }
+
+
+  //method to fetch Githb users data from REST Api by making http request
+  getGithubUserDetails(userTrueName) {
+    return new Promise(resolve => {
+     // console.log(userTrueName);
+      this.httpServ.get("https://api.github.com/users/" + userTrueName + "/repos")
+        .map(resp => resp.json())
+        .subscribe(tempdata => {
+          this.gitUserDetails = tempdata;
+          resolve(this.gitUserDetails);
+        });
+    });
+  }
+
+}
+
