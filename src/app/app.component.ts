2a558ad64271a9b89c0ec8c28fee9bdccdb6ef62
TheBeastNk <niranjans97@gmail.com>
2018-11-07 22:40:14 +0530

    service created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  inputName: string = "";
+  totalUserCount: number;
+  userDataSet: any;
+  userDataItems: any = [];
+  userDetails: any = [];
+  selectedSort: string = "NameAlphabeticallyAZ";
 
+  filteredItems: any = [];
+  pages: number;
+  pageSize: number = 3;
+  pageNumber: number = 0;
+  currentIndex: number = 1;
+  pagesIndex: Array<number>;
+  pageStart: number = 1;
+  
+  //Sorting method
+  SortingResult() {
+    if (this.selectedSort === "NameAlphabeticallyAZ") {
+      this.userDataItems.sort();
+    }
+    else if (this.selectedSort === "NameReverseAlphabeticallyZA") {
+      this.userDataItems.reverse();
+    }
+    else if (this.selectedSort === "RankUp") {
+      this.userDataItems.sort((a, b) => {
+        return a.score < b.score;
+      });
+    }
+    else if (this.selectedSort === "RankDown") {
+      this.userDataItems.sort((a, b) => {
+        return a.score > b.score;
+      });
+    }
+
+  }
 }
